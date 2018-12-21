2e5fcb39089fb45c55d494852e8234c2161dbed0
John Werenski <jtwerenski@gmail.com>
2018-12-21 18:19:10 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -33,6 +33,7 @@ class OwnersController < ApplicationController
     if !params[:owner].keys.include?("pet_ids")
       params[:owner][:pet_ids] = []
     end 
+    
     @owner = Owner.find(params[:id])
     @owner.update(params[:owner])
    
