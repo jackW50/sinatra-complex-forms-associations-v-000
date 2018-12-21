e53bb6b07bc19e7463be4803bb0c958213393669
John Werenski <jtwerenski@gmail.com>
2018-12-21 18:32:41 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -28,6 +28,7 @@ class PetsController < ApplicationController
     @pet.update(params[:pet])
     if !params[:owner][:name].empty?
       @pet.owner = Owner.new(params[:owner])
+    end 
     redirect to "pets/#{@pet.id}"
   end
 end
