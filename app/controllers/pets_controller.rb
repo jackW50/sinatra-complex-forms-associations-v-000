33bb13cb88b34795b2d4bb627ca04909c69f98f0
John Werenski <jtwerenski@gmail.com>
2018-12-21 18:57:56 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -23,7 +23,10 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
   
-  get '/pets/:id/edit'
+  get '/pets/:id/edit' do 
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end 
 
   patch '/pets/:id' do 
     @pet = Pet.find(params[:id])
