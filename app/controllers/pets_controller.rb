bc0b8984b92f2df9510cd1db825f037420e462a8
John Werenski <jtwerenski@gmail.com>
2018-12-21 19:03:37 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,7 +10,8 @@ class PetsController < ApplicationController
     erb :'/pets/new'
   end
 
-  post '/pets' do 
+  post '/pets' do
+    binding.pry
     @pet = Pet.create(params[:pet])
     if !params[:owner][:name].empty?
       @pet.owner = Owner.new(params[:owner])
